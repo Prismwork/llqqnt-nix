8dfac1a5a381ee547da497298786107a070213d3
Flamarine <William27060827@hotmail.com>
2023-10-01 13:22:20 +0800

    nix好难

--- flake.nix
+++ flake.nix
@@ -1,17 +1,17 @@
 {
   description = "LLQQNT Flake";
   inputs = {
-    nixpkgs = {
-      url = github:NixOS/nixpkgs/nixos-unstable;
-      inputs.allowUnfree = true;
-    };
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
     flake-utils.url = github:numtide/flake-utils;
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
         
         pkgsLocal = import ./pkgs { inherit pkgs; };
       in {
