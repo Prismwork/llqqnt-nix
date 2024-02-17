47a52d65c9811c92a3fceee3361b55ac1428ff9f
Flamarine <William27060827@hotmail.com>
2024-02-17 14:21:33 +0800

    lock rev again

--- pkgs/llqqnt.nix
+++ pkgs/llqqnt.nix
@@ -1,16 +1,14 @@
 { pkgs, ... }:
 
 let
-  llVersion = "1.0.3";
   llSrc = builtins.fetchGit {
     url = "https://github.com/LiteLoaderQQNT/LiteLoaderQQNT";
-    rev = "refs/tags/v${llVersion}";
+    rev = "7cd4b41f88d606cb2134ea2b23d9ce31dc3242f7";
     submodules = true;
   };
 in
 pkgs.qq.overrideAttrs (oldAttrs: {
   pname = "llqqnt";
-  version = "${oldAttrs.version}+llqqnt.${llVersion}";
 
   postInstall = ''
     substituteInPlace $out/opt/QQ/resources/app/app_launcher/index.js --replace 'require\(".*/launcher\.node"\)' 'require("${llSrc.outPath}");\n  require("./launcher.node")'
